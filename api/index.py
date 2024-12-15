db9d409446331ee77f0a187824554075df146b40
shepwarlock <shephardwarlock@gmail.com>
2024-12-15 00:07:42 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,6 @@
-import requests
-from http.server import BaseHTTPRequestHandler
- 
-class handler(BaseHTTPRequestHandler):
- 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+   # api/hello.py
+   def handler(request, event):
+       return {
+           "statusCode": 200,
+           "body": "Hello from Python!"
+       }
