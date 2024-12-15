63d20d784e2e13d3f5c1d347e12507b1547d521f
shepwarlock <shephardwarlock@gmail.com>
2024-12-15 00:15:26 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,9 +8,4 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/plain')
         self.end_headers()
         self.wfile.write('Hello, world!'.encode('utf-8'))
-        return   # api/hello.py
-   def handler(request, event):
-       return {
-           "statusCode": 200,
-           "body": "Hello from Python!"
-       }
+        return
